de1b18e68d633c46925f20757d3918e84bf7eb1b
Jinip <jnperciv@gmail.com>
2016-12-05 14:25:18 -0700

    Added templates for other page views

--- public/js/app.js
+++ public/js/app.js
@@ -10,6 +10,18 @@ app.config(["$routeProvider", function ($routeProvider) {
             templateUrl: "js/components/blog/blog.html",
             controller: "BlogController"
         })
+        .when("/shop", {
+            templateUrl: "js/components/shop/shop.html",
+            controller: "ShopController"
+        })
+        .when("/pricing", {
+            templateUrl: "js/components/pricing/pricing.html",
+            controller: "PricingController"
+        })
+        .when("/about", {
+            templateUrl: "js/components/about/about.html",
+            controller: "AboutController"
+        })
         .otherwise("/", {
             templateUrl: "js/components/landing-page/landing-page.html",
             controller: "LandingPageController"
